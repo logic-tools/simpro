4e7c259de39dab4c6ea7d66e6961fdf27a164722
Jørgen Villadsen <joergenvilladsen@users.noreply.github.com>
2016-05-13 20:16:18 +0200

    Tuned X

---
+++ X.thy
@@ -0,0 +1,7 @@
+theory X imports SimPro begin
+
+ML {* val true = X.check (X.Dis (X.Pre (true,X.Zero_nat,[]),X.Pre (false,X.Zero_nat,[]))) *}
+
+(* ML {* val true = X.check (X.Pre (true,X.Zero_nat,[])) *} *)
+
+end
